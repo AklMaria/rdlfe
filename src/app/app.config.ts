4067c9606ff0e9fc1a8e54561dfc8ba3862d0ec0
gian-lu11 <gianlucaursino13@gmail.com>
2025-11-10 21:57:24 +0100

    fix auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,60 +1,48 @@
-import {ApplicationConfig, importProvidersFrom, LOCALE_ID, provideAppInitializer} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, LOCALE_ID, provideAppInitializer } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { routes } from './app.routes';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
-import { GoogleLoginProvider, FacebookLoginProvider } from '@abacritt/angularx-social-login';
-import {environment} from "./environment";
-import {provideRouter} from "@angular/router";
-import {provideHttpClient} from "@angular/common/http";
-import {getAuth, provideAuth} from "@angular/fire/auth";
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { registerLocaleData } from '@angular/common'; // <-- Importa questo
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from './environment';
+import { SocialAuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider } from '@abacritt/angularx-social-login';
+import { registerLocaleData } from '@angular/common';
 import localeIt from '@angular/common/locales/it';
-import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'it' },
+
+    // 🔐 Social Login
     {
-      provide: LOCALE_ID,
-      useValue: 'it'
-    },
-    {
-      provide: "SocialAuthServiceConfig",
+      provide: 'SocialAuthServiceConfig',
       useValue: {
         autoLogin: false,
         providers: [
           {
             id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              environment.GOOGLE_CLIENT_ID
-            )
+            provider: new GoogleLoginProvider(environment.GOOGLE_CLIENT_ID),
           },
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider(
-              environment.FB_CLIENT_ID
-            )
-          }
+            provider: new FacebookLoginProvider(environment.FB_CLIENT_ID),
+          },
         ],
-        onError: (err) => {
-          console.error(err);
-        }
+        onError: (err) => console.error(err),
       } as SocialAuthServiceConfig,
     },
 
-    provideAnimationsAsync(),
     provideRouter(routes),
     provideHttpClient(),
+
+    // ✅ Firebase init
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
-    importProvidersFrom(
-      BrowserModule,
-      ReactiveFormsModule,
-    ),
-    provideAppInitializer(() => {
-      registerLocaleData(localeIt);
-    }),
-  ]
+
+    importProvidersFrom(BrowserModule, ReactiveFormsModule),
+
+    provideAppInitializer(() => registerLocaleData(localeIt)),
+  ],
 };
